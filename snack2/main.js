d1ecdfaff54e85fcf6d810997fe68ff75240cc4c
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-04 17:00:51 +0100

    esercizio 2

--- snack2/main.js
+++ snack2/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-  let numeri = ['pippo', 'pluto', 'paperino', 'paperone', 'qui', 'quo', 'qua']
+  let nomi = ['pippo', 'pluto', 'paperino', 'paperone', 'qui', 'quo', 'qua']
 
+   // chiedo due numeri all'utente
+
+  let numero1 = parseInt(prompt('inserisci un numero da 0 a 7'))
+
+  let numero2 = parseInt(prompt('inserisci un altro numero da 0 a 7'))
+
+  console.log(numero1);
+
+  console.log(numero2);
+
+  const nomiEstratti = nomi.filter((element, index) => {
+  return numero1 <= index && numero2 >= index
+
+  })
+
+  console.log(nomiEstratti);
 
 
 })
